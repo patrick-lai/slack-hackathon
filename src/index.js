a9cb50c7203fb39d941d367d5c69d41b9746fb94
Patrick <mrphlai@gmail.com>
2019-03-08 10:43:22 +1100

    Initi

---
+++ src/index.js
@@ -0,0 +1,29 @@
+/**
+ * Secret Fuhrer game engine
+ */
+
+// Join game
+
+// Assign role
+
+// Start game (anyone)
+
+// Random president
+
+// President picks cancellor
+
+// Vote
+
+// Turn
+
+// VOTING BLOCK
+
+// If voted in president gets 3 cards off the stack and he picks 2
+
+// Chancellor picks 1 of the remaining
+
+// VOTING BLOCK
+
+// Check if game is won
+
+// Repeat turn
